ec108eeb31fab44e5659627ab16eb326edbae55f
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-12-10 10:16:38 +0900

    feat : 타격 이펙트 추가

--- Assets/02. Scripts/Player/PlayerGunFire.cs
+++ Assets/02. Scripts/Player/PlayerGunFire.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 public class PlayerGunFire : MonoBehaviour
 {
     [SerializeField] private Transform _fireTransform;
+    [SerializeField] private ParticleSystem _hitEffectPrefab;
     private void Update()
     {
         // 1. 마우스 왼쪽 버튼이 눌린다면
         if (Input.GetMouseButtonDown(0))
         {
             // 2. Ray를 생성하고 발사할 위치, 방향, 거리를 설정한다.
-            Ray ray = new Ray(_fireTransform.position, _fireTransform.forward);
-            Debug.DrawRay(_fireTransform.position, _fireTransform.forward * 100f, Color.red, 2f);
+            Ray ray = new Ray(_fireTransform.position, Camera.main.transform.forward);
+            Debug.DrawRay(_fireTransform.position, Camera.main.transform.forward * 100f, Color.red, 2f);
             // 3. RayCasetHit(충돌한 대상의 정보)를 저장할 변수를 생성한다.
             RaycastHit hitInfo = new RaycastHit();
 
@@ -21,6 +22,8 @@ public class PlayerGunFire : MonoBehaviour
             {
                 //5. 충돌했다면... 피격 이펙트 표시
                 Debug.Log($"Hit : {hitInfo.transform.name} ");
+                ParticleSystem hitEffect = Instantiate(_hitEffectPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                hitEffect.Play();
             }
         }
 
