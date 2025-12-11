f029577372e246c48ea14ced8ead864236fe636c
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-12-11 16:21:53 +0900

    feat : Bomb Attack Damage Apply

--- Assets/02. Scripts/Player/Bomb/Bomb.cs
+++ Assets/02. Scripts/Player/Bomb/Bomb.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     public GameObject _explosionEffectPrefab;
+    [SerializeField] private float _damage = 40;
     private void OnCollisionEnter(Collision collision)
     {
         Instantiate(_explosionEffectPrefab,transform.position, Quaternion.identity);
 
         gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Monster monster = other.GetComponent<Monster>();
+        if (monster != null)
+        {
+            monster.TryTakeDamage(_damage, Vector3.up);
+        }
     }
 }
