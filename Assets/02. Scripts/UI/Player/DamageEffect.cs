7e4dac1a68969d97bafb026c46045ac73d007c45
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-12-15 17:06:50 +0900

    feat : Health Bar - 시 뒤에 따라오는 빨간 바

--- Assets/02. Scripts/UI/Player/DamageEffect.cs
+++ Assets/02. Scripts/UI/Player/DamageEffect.cs
@@ -8,6 +8,7 @@ public class DamageEffect : MonoBehaviour
     private Image _image;
     private Player _player;
     private Color _color = new Color(1, 1, 1, 0.5f);
+    private Color _originColor = Color.clear;
 
     private void Start()
     {
@@ -16,9 +17,12 @@ public class DamageEffect : MonoBehaviour
         _player.HealthChanged += DamageEffectPlay;
     }
 
-    public void DamageEffectPlay(float obj)
+
+    public void DamageEffectPlay(PlayerStats stats)
     {
-        Debug.Log("yes");
+        _image.DOKill();
+        _image.color = _originColor;
+
         _image.DOColor(_color, 0.3f).SetEase(Ease.OutSine).SetLoops(2, LoopType.Yoyo);
     }
 }
