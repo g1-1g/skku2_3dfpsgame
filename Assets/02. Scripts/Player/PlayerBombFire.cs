16165fcac9d308617668686aee077831905c3b10
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-12-16 12:36:23 +0900

    refactor : PlayerBombFire 리팩토링

--- Assets/02. Scripts/Player/PlayerBombFire.cs
+++ Assets/02. Scripts/Player/PlayerBombFire.cs
@@ -25,12 +25,9 @@ public class PlayerBombFire : MonoBehaviour
 
             GameObject bomb = BombFactory.Instance.MakeBomb(_FireTransform.transform.position);
             if (bomb == null) return;
-            bomb.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            bomb.GetComponent<Rigidbody>().AddForce(_camera.transform.forward * ThrowPower, ForceMode.Impulse);
-            Debug.DrawRay(_camera.transform.position,
-
-              _camera.transform.forward * 3f,
-              Color.red, 2f);
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+            rb.angularVelocity = Vector3.zero;
+            rb.AddForce(_camera.transform.forward * ThrowPower, ForceMode.Impulse);
             _chance--;
             OnBombCreated?.Invoke(_chance);
         }
